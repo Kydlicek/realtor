53634c636097fb17777ccd4129d6bdaa407848fd
Kydlíček <krystof.kydlicek@siemens.com>
2024-01-02 11:57:47 +0100

    new class scrape

--- url_scraper.py
+++ url_scraper.py
@@ -1,57 +1,6 @@
-import requests
-from bs4 import BeautifulSoup
-import json
-import time as tm
-from Class import RealEstateListing, Database
+from Class import RealEstateListing, Database, Scrape
 
-#function to get page
-def get_page(url):
-    
-    res = requests.get(url)
-    if res.status_code == 200:
-            soup = BeautifulSoup(res.content,'html.parser')
-            soup = json.loads(soup.text)
-            return soup
-    else:
-        print(f'Error {res.status_code}')
 
-#function to get lenght of all results on target side
-def get_res_lenght():
-    p = get_page(url)
-    return p['result_size']
-
-db = Database('real_estate_app','first_scrape')
-
-#category_main = 1 Byty, 2 Domy, 3 Pozemky, 4 Komercni, 5 Ostatni
-category_main = 1
-#category_type = 1 Prodej, 2 Pronajem
-category_type = 1
-#page_num = 1 = cislo stranky
-page_num = 1
-#per_page = 60 = pocet inzeratu na strance
-per_page = 60
-#aktualni cas v milisekundach
-tms = tm.time()
-
-url = f'https://www.sreality.cz/api/cs/v2/estates?category_main_cb={category_main}&category_type_cb={category_type}&no_auction=1&no_shares=1&page={page_num}&per_page={per_page}&tms={tms}'
-
-#list of all properties
-all_props = []
-res_lenght = get_res_lenght()
-pages_to_scrape = res_lenght // per_page + 1
-
-#loop through pages
-for i in range(1,pages_to_scrape+1):
-    print(f'Scraping page {i} of {pages_to_scrape}')
-    page_num = i
-    soup = get_page(url)
-    props  = soup['_embedded']['estates']
-    
-    #page return 60 listing > loop iteration to get all listings on page
-    for prop in props:
-        prop = RealEstateListing(prop)
-        all_props.append(prop.dic)
-
-#insert all properties into database
-db.insert_many(all_props)
-print(f'Inserted {len(all_props)} documents into database')
+flat_rentals = Scrape(1, 2, "flat_rentals_urls")
+flat_rentals.scrape_all()
+flat_rentals.add_to_db()
